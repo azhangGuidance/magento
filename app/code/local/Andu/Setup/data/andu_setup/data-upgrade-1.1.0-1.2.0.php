b8960fa87aac61b0ff82435ff796ea929719af80
Andu Zhang <azhan@guidance.com>
2014-10-03 01:51:50 -0700

    update

--- app/code/local/Andu/Setup/data/andu_setup/data-upgrade-1.1.0-1.2.0.php
+++ app/code/local/Andu/Setup/data/andu_setup/data-upgrade-1.1.0-1.2.0.php
@@ -9,13 +9,16 @@ $rootCategoryId = Mage::app()->getStore()->getRootCategoryId();
 $rootCategory = Mage::getModel('catalog/category')->load($rootCategoryId);
 $categories = array('Restaurant','Coffee Bar & Tea House','Bars','Rental');
 $path = $rootCategory->getPath();
-foreach($categories as $category){
-    $category = Mage::getModel('catalog/category')
-        ->setName($category)
-        ->setIsActive(true)
-        ->setPath($path)
-        ->setIncludeInMenu(1)
-        ->save();
+$store = Mage::getModel('core/store')->load('default', 'code');
+if($store && $store->getId()){
+    foreach($categories as $category){
+        $category = Mage::getModel('catalog/category')->setStoreId($store->getId())
+            ->setName($category)
+            ->setIsActive(true)
+            ->setPath($path)
+            ->setIncludeInMenu(1)
+            ->save();
+    }
 }
 Mage::unregister('isSecureArea');
 $installer->endSetup();
