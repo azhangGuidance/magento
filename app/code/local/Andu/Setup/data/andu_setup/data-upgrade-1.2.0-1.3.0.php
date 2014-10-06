a291c6c9ca412c8b63d7e27a6d88f392ed9fb02a
Andu Zhang <azhan@guidance.com>
2014-10-05 19:15:54 -0700

    translation update

--- app/code/local/Andu/Setup/data/andu_setup/data-upgrade-1.2.0-1.3.0.php
+++ app/code/local/Andu/Setup/data/andu_setup/data-upgrade-1.2.0-1.3.0.php
@@ -4,5 +4,6 @@ $installer = $this;
 $installer->startSetup();
 $installer->setConfigData('design/package/name', 'default', 'default', 0);
 $installer->setConfigData('design/theme/default', 'f001', 'default', 0);
+$installer->setConfigData('cataloginventory/item_options/manage_stock', 0, 'default', 0);
 
 $installer->endSetup();
