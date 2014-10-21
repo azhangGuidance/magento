89057d2d0542b82edb54167d6bcfaf7410fd72a5
Andu Zhang <azhan@guidance.com>
2014-10-20 17:52:03 -0700

    customer module

---
+++ app/code/local/Andu/Customer/controllers/AccountController.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("Mage/Customer/controllers/AccountController.php");
+class Andu_Customer_AccountController extends Mage_Customer_AccountController
+{
+
+    protected function _welcomeCustomer(Mage_Customer_Model_Customer $customer, $isJustConfirmed = false)
+    {
+        $this->_getSession()->addSuccess(
+            $this->__('Thank you for registering with us.')
+        );
+        if ($this->_isVatValidationEnabled()) {
+            // Show corresponding VAT message to customer
+            $configAddressType =  $this->_getHelper('customer/address')->getTaxCalculationAddressType();
+            $userPrompt = '';
+            switch ($configAddressType) {
+                case Mage_Customer_Model_Address_Abstract::TYPE_SHIPPING:
+                    $userPrompt = $this->__('If you are a registered VAT customer, please click <a href="%s">here</a> to enter you shipping address for proper VAT calculation',
+                        $this->_getUrl('customer/address/edit'));
+                    break;
+                default:
+                    $userPrompt = $this->__('If you are a registered VAT customer, please click <a href="%s">here</a> to enter you billing address for proper VAT calculation',
+                        $this->_getUrl('customer/address/edit'));
+            }
+            $this->_getSession()->addSuccess($userPrompt);
+        }
+
+        $customer->sendNewAccountEmail(
+            $isJustConfirmed ? 'confirmed' : 'registered',
+            '',
+            Mage::app()->getStore()->getId()
+        );
+
+        $successUrl = $this->_getUrl('*/*/index', array('_secure' => true));
+        if ($this->_getSession()->getBeforeAuthUrl()) {
+            $successUrl = $this->_getSession()->getBeforeAuthUrl(true);
+        }
+        return $successUrl;
+    }
+}
+?>
